bc25b864b7e31f18802b26c8376ed9b57e562e64
Adityaa_ <aaditya85677@gmail.com>
2025-09-21 16:44:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ['g76qsn-5173.csb.app'], // ✅ yaha likhna hai
+    port: 5173, // optional: agar specific port chahiye
+  },
 })
