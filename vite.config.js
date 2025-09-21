13e2032ecd30337dd2255fe71fd9e0449ef5901e
Adityaa_ <aaditya85677@gmail.com>
2025-09-21 16:52:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      'g76qsn-5173.csb.app', // yeh host allow karna hai
+    ],
+  },
 })
